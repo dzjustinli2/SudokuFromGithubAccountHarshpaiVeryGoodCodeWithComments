bac834340cd05d021c24f2d9f071bc5528c15d06
Justin LI <dzjustinli2@gmail.com>
2016-08-08 21:16:48 +1000

    Initial Commit

---
+++ src/sudokufromgithubaccountharshpai/ListOfTodos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokufromgithubaccountharshpai;
+
+/**
+ *
+ * @author justin
+ */
+public class ListOfTodos {
+
+}
+
+//TODO: 1) find out why we have to copy the array "a" here instead of just returning it. My guess is that if we return array "a",
+//we are simply returning a pointer to it. But even if we return array "result", are we not simply returning a pointer to array "result"
+//as well? If in both instance, we are returning a pointer, then what is the difference anyway? Require some serious thinking 
